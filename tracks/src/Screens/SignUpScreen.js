1cd6db6a605cdbbcaca6ed6319db7807c46d3505
bhvbhushan <bhavya.bhushan@elucidata.io>
2020-01-12 14:31:35 +0530

    [WIP] Add SignUp Screen layout using react-native-element

--- tracks/src/Screens/SignUpScreen.js
+++ tracks/src/Screens/SignUpScreen.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
 
 const SignUpScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 48 }}> SignUpScreen</Text>
-      <Button
-        title='Go To SignIn'
-        onPress={() => navigation.navigate('SignIn')}
-      />
-      <Button
-        title='Go To MainFlow'
-        onPress={() => navigation.navigate('TrackList')}
-      />
+      <Text h3>Sign Up for Tracker</Text>
+      <Input label='Email' />
+      <Input label='Password' />
+      <Button title='Sign Up' />
     </>
   );
 };
