a71dc50d03acd94224776ed347a0860bfa49bd4e
bhvbhushan <bhavya.bhushan@elucidata.io>
2020-01-12 20:58:53 +0530

    Add Global COntext

--- tracks/App.js
+++ tracks/App.js
@@ -9,6 +9,7 @@ import SignUpScreen from './src/Screens/SignUpScreen';
 import TrackCreateScreen from './src/Screens/TrackCreateScreen';
 import TrackDetailScreen from './src/Screens/TrackDetailScreen';
 import TrackListScreen from './src/Screens/TrackListScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext';
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -25,4 +26,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
