69107984b8f82f8816d63b730219ea9c52cb2182
bhvbhushan <bhavya.bhushan@elucidata.io>
2020-01-13 11:59:39 +0530

    [WIP] Add token to local storage

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,27 +1,29 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { AsyncStorage } from 'react-native';
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_err':
       return { ...state, errMessage: action.payload };
+    case 'signup':
+      return { errMessage: '', token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = dispatch => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
-    } catch (err) {
-      dispatch({
-        type: 'add_err',
-        payload: 'Something went wrong with the signup'
-      });
-    }
-  };
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
+  } catch (err) {
+    dispatch({
+      type: 'add_err',
+      payload: 'Something went wrong with the signup'
+    });
+  }
 };
 
 const signin = dispatch => {
@@ -35,5 +37,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errMessage: '' }
+  { token: null, errMessage: '' }
 );
