c4afa83771e6e499071d1a1ed0f7254d400e111d
bhvbhushan <bhavya.bhushan@elucidata.io>
2020-01-13 23:56:36 +0530

    [WIP] Use maps to with dummy points

--- tracks/src/Screens/TrackCreateScreen.js
+++ tracks/src/Screens/TrackCreateScreen.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import Map from '../components/Map';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}> TrackCreateScreen</Text>;
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      console.log(e);
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+      {err ? <Text> Please enable location service</Text> : null}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
